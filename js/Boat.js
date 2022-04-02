8e80951ce5c1fefa6b37b8ce89277b7b4f392dd8
Matasor <96345204+Matasor@users.noreply.github.com>
2022-04-02 12:32:02 -0300

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,36 @@
+class Boat {
+  constructor(x, y, width, height, boatPos,animationboat) {
+  
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.width = width;
+    this.height = height;
+    this.boatPosition = boatPos;
+    this.image = loadImage("./assets/boat.png");
+this.animation = animationboat;
+    World.add(world, this.body);
+  }
+
+  remove(index) {
+    setTimeout(() => {
+      Matter.World.remove(world, boats[index].body);
+      delete boats[index];
+    }, 0);
+  }
+
+  animate(){
+    this.speed += 0.5;
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+    var index = floor(this.speed % this.animation.lenght)
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.animation[index] , 0, this.boatPosition, this.width, this.height);
+    pop();
+  }
+}
+
